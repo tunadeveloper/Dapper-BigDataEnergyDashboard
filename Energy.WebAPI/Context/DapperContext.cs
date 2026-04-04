5e237d6920783fe0b35a0175dd9c6c090bd6a1c3
tunadeveloper <tunabusiness25@gmail.com>
2026-04-04 18:41:04 +0300

    Updated

---
+++ Energy.WebAPI/Context/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Energy.WebAPI.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configration)
+        {
+            _configration = configration;
+            _connectionString = _configration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
