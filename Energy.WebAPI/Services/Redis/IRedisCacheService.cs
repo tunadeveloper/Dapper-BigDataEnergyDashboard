b6151a786f81748491df4c0f8d0e4327dfc419f5
tunadeveloper <tunabusiness25@gmail.com>
2026-04-04 18:36:50 +0300

    Updated

--- Energy.WebAPI/Services/Redis/IRedisCacheService.cs
+++ Energy.WebAPI/Services/Redis/IRedisCacheService.cs
@@ -2,5 +2,8 @@
 {
     public interface IRedisCacheService
     {
+        T GetData<T>(string key);
+        void SetData<T>(string key, T value, TimeSpan expirationTime);
+        void RemoveData(string key);
     }
 }
--- Energy.WebAPI/Services/Redis/RedisCacheService.cs
+++ Energy.WebAPI/Services/Redis/RedisCacheService.cs
@@ -1,6 +1,30 @@
-﻿namespace Energy.WebAPI.Services.Redis
+﻿using StackExchange.Redis;
+
+namespace Energy.WebAPI.Services.Redis
 {
-    public class RedisCacheService
+    public class RedisCacheService : IRedisCacheService
     {
+        private readonly IDatabase _database;
+
+        public RedisCacheService()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            _database = redis.GetDatabase();
+        }
+
+        public T GetData<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveData(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetData<T>(string key, T value, TimeSpan expirationTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
