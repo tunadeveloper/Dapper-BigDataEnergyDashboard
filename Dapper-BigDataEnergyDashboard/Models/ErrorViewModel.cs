e4dd266df5d82d2951b5902c4436bf9e22f3984f
tunadeveloper <tunabusiness25@gmail.com>
2026-04-04 18:18:13 +0300

    Updated

--- Dapper-BigDataEnergyDashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Dapper_BigDataEnergyDashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
