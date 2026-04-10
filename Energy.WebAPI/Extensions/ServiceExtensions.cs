02e01783d5a027bfa2ed4c73ebd00d3f18ac8daa
tunadeveloper <tunabusiness25@gmail.com>
2026-04-10 14:54:35 +0300

    Updated

--- Energy.WebAPI/Extensions/ServiceExtensions.cs
+++ Energy.WebAPI/Extensions/ServiceExtensions.cs
@@ -1,10 +1,42 @@
-﻿using Energy.WebAPI.Redis;
+﻿using Energy.WebAPI.Context;
+using Energy.WebAPI.Repositories.Dashboard;
+using Energy.WebAPI.Repositories.MeterReadings;
+using Energy.WebAPI.Repositories.Meters;
+using Energy.WebAPI.Repositories.Regions;
+using Energy.WebAPI.Services;
+using Energy.WebAPI.Redis;
 using StackExchange.Redis;
 
 namespace Energy.WebAPI.Extensions
 {
     public static class ServiceExtensions
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("SignalRClient", policy =>
+                {
+                    policy
+                        .SetIsOriginAllowed(_ => true)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+            services.AddSignalR();
+            services.AddOpenApi();
+            services.AddScoped<DapperContext>();
+            services.AddRedisCache(configuration);
+            services.AddScoped<IMeterService, MeterService>();
+            services.AddScoped<IRegionService, RegionService>();
+            services.AddScoped<IMeterReadingService, MeterReadingService>();
+            services.AddScoped<IDashboardRepository, DashboardRepository>();
+            services.AddHostedService<RedisPulseBroadcastService>();
+            return services;
+        }
+
         public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
         {
             var redisConfig = configuration["Redis:Configuration"];
--- Energy.WebAPI/Program.cs
+++ Energy.WebAPI/Program.cs
@@ -1,36 +1,10 @@
-using Energy.WebAPI.Context;
 using Energy.WebAPI.Extensions;
 using Energy.WebAPI.Hubs;
-using Energy.WebAPI.Repositories.Dashboard;
-using Energy.WebAPI.Repositories.MeterReadings;
-using Energy.WebAPI.Repositories.Meters;
-using Energy.WebAPI.Repositories.Regions;
-using Energy.WebAPI.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("SignalRClient", policy =>
-    {
-        policy
-            .SetIsOriginAllowed(_ => true)
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
-});
-builder.Services.AddSignalR();
-builder.Services.AddOpenApi();
-builder.Services.AddScoped<DapperContext>();
-builder.Services.AddRedisCache(builder.Configuration);
-builder.Services.AddScoped<IMeterService, MeterService>();
-builder.Services.AddScoped<IRegionService, RegionService>();
-builder.Services.AddScoped<IMeterReadingService, MeterReadingService>();
-builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
-builder.Services.AddHostedService<RedisPulseBroadcastService>();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
